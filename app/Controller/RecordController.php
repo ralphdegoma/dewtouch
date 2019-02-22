70297415985676c50136bba7c290720ed60cea5f
ralph <degomaralphreigh@gmail.com>
2019-02-23 06:30:42 +0800

    first commit

---
+++ app/Controller/RecordController.php
@@ -0,0 +1,19 @@
+<?php
+	class RecordController extends AppController{
+		
+		public function index(){
+			ini_set('memory_limit','256M');
+			set_time_limit(0);
+			
+			$this->setFlash('Listing Record page too slow, try to optimize it.');
+			
+			
+			$records = $this->Record->find('all');
+			
+			$this->set('records',$records);
+			
+			
+			$this->set('title',__('List Record'));
+		}
+
+	}
